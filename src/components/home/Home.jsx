39e31306a654cc3c985f5e0f59b3338e7ac5ebf1
Jorge Lujan <jlujan@ika.mx>
2025-06-02 11:20:17 -0600

    yo

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,38 +7,32 @@ import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
-        
-        <Section >
-              <Helmet defer={false}>
+        <Section>
+            <Helmet defer={false}>
                 <title>IKA - Home</title>
                 <meta name="description" content="IKA es una empresa mexicana especializada en consultoría y soporte de TI." />
             </Helmet>
 
-              <MainTitle text="Bienvenidos a IKA" className="typing-title"/>
-          
-        <ScrollableSection className="home-container" index={0}>
-          
-            
-            <p>
-               <strong className='ika'>IKA</strong>  es una empresa mexicana que nace en 2001, con la idea de prestar servicios de consultoría y soporte a las empresas de la ciudad de Chihuahua. Está conformada por un grupo de profesionales con más de 20 años de experiencia en distintas áreas de la Tecnología de la Información, con una visión enfocada a apoyar y fomentar el uso de tecnologías libres.
-            </p>
-            <p>
-                Ofrecemos servicios informáticos, consultorías, soporte técnico, desarrollo de proyectos especiales, venta y renta de equipo de cómputo. Nuestro objetivo es ayudar a las empresas a alcanzar sus metas tecnológicas de manera eficiente y sostenible.
-            </p>
-            <p>
-                Contamos con ingenieros capacitados en distintas ramas de la tecnología, lo que nos permite contar con un portafolio de servicios amplio y bases sólidas para satisfacer los estándares de calidad exigidos por nuestros clientes. Participamos activamente en sus proyectos, llevándolos a buen término y cumpliendo cabalmente con sus necesidades.
-            </p>
-           
+            <MainTitle text="Bienvenidos a IKA" className="typing-title" />
 
-        
-        <button className="request">
-            <CallToAction content="Solicita una consulta"/>
-            </button>
+            <ScrollableSection className="home-container" index={0}>
+                <div className="content-columns">
+                    <p>
+                        <strong className='ika'>IKA</strong> es una empresa mexicana que nace en 2001, con la idea de prestar servicios de consultoría y soporte a las empresas de la ciudad de Chihuahua. Está conformada por un grupo de profesionales con más de 20 años de experiencia en distintas áreas de la Tecnología de la Información, con una visión enfocada a apoyar y fomentar el uso de tecnologías libres.
+                    </p>
+                    <p>
+                        Ofrecemos servicios informáticos, consultorías, soporte técnico, desarrollo de proyectos especiales, venta y renta de equipo de cómputo. Nuestro objetivo es ayudar a las empresas a alcanzar sus metas tecnológicas de manera eficiente y sostenible.
+                    </p>
+                    <p>
+                        Contamos con ingenieros capacitados en distintas ramas de la tecnología, lo que nos permite contar con un portafolio de servicios amplio y bases sólidas para satisfacer los estándares de calidad exigidos por nuestros clientes. Participamos activamente en sus proyectos, llevándolos a buen término y cumpliendo cabalmente con sus necesidades.
+                    </p>
+                </div>
+
+  
+                    <CallToAction content="Solicita una consulta" className="request"/>
+
             </ScrollableSection>
         </Section>
-        
-        
-
     );
 };
 
