9a80582f1d3f7ea13e16161f7e1d7b42c8aa7bcd
Jorge Lujan <jlujan@ika.mx>
2025-06-02 11:47:19 -0600

    yeahh

--- src/components/ui/header/Header.jsx
+++ src/components/ui/header/Header.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import logo from "../../../assets/logo.png";
+import logo from "../../../assets/lyr.jpg";
 import HeaderMovil from "../headerMovil/HeaderMovil";
 
 const navLinks = [
